15f134c512fb3841b2595e00452a28a62f62795a
nahid <nahidferdous240@gmail.com>
2026-05-23 15:10:22 +0600

    .env update

--- src/routes/_app/quizzes/$id/result-pages/index.tsx
+++ src/routes/_app/quizzes/$id/result-pages/index.tsx
@@ -226,7 +226,7 @@ export default function RouteComponent() {
                   collisionDetection={closestCorners}
                 >
                   <SortableContext
-                    items={localPages.map((p) => p.id!)}
+                    items={localPages.map((p) => p.id as string | number)}
                     strategy={verticalListSortingStrategy}
                   >
                     <TableBody>
